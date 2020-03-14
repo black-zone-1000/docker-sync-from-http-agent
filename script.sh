56fa76e8db8c062f0dadedee47b800e89fff2af8
xmaster <xmaster@xmasterpc.localdomain>
2020-03-14 20:41:03 +0200

    Fix script

--- script.sh
+++ script.sh
@@ -2,26 +2,26 @@
 . /env.sh
 
 echo "$(date): executed script"
-echo "REMOTE_HOST=${REMOTE_HOST}"
-echo "TARGET_FOLDER=${TARGET_FOLDER}"
-echo "SOURCE_FOLDER=${SOURCE_FOLDER}"
+echo "REMOTE_HOST=${REMOTE_HOST[*]}"
+echo "TARGET_FOLDER=${TARGET_FOLDER[*]}"
+echo "SOURCE_FOLDER=${SOURCE_FOLDER[*]}"
 
 typeset -i i=0 max=${#REMOTE_HOST[*]}
 
 while (( i < max ))
 do
-	HOST=${REMOTE_HOST[$i]}
-	TARGETFOLDER=${TARGET_FOLDER[$i]}
-	SOURCEFOLDER=${SOURCE_FOLDER[$i]}
+	_HOST=${REMOTE_HOST[$i]}
+	_TARGETFOLDER=${TARGET_FOLDER[$i]}
+	_SOURCEFOLDER=${SOURCE_FOLDER[$i]}
 
-	echo "syncing from ${HOST}:${SOURCE_FOLDER} to ${TARGET_FOLDER}"
+	echo "syncing from ${_HOST}:${_SOURCEFOLDER} to ${_TARGETFOLDER}"
 
 	mkdir ${TARGET_FOLDER}
 
 	lftp -f "
-		open $HOST
+		open $_HOST
 		set ftp:list-options -a
-		mirror -c -e $SOURCEFOLDER $TARGETFOLDER
+		mirror -c -e $_SOURCEFOLDER $_TARGETFOLDER
 		bye
 	"
 	echo "syncing complete"
